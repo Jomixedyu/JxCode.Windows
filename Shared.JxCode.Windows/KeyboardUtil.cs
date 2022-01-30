d04370edb06711a5c2f800dfc166e525da6c36f7
JomiXedYu <JomiXedYu@outlook.com>
2022-01-31 02:31:21 +0800

    Update KeyboardUtil.cs

--- Shared.JxCode.Windows/KeyboardUtil.cs
+++ Shared.JxCode.Windows/KeyboardUtil.cs
@@ -4,6 +4,14 @@ namespace JxCode.Windows
 {
     public static class KeyboardUtil
     {
+        public static void Down(User32.VK_Keys key)
+        {
+            User32.keybd_event(key, 0, User32.KeyEvent.down, 0);
+        }
+        public static void Up(User32.VK_Keys key)
+        {
+            User32.keybd_event(key, 0, User32.KeyEvent.up, 0);
+        }
         public static void Click(User32.VK_Keys key)
         {
             User32.keybd_event(key, 0, User32.KeyEvent.down, 0);
